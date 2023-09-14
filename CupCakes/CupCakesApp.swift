ca81ed31bcdd96ebb93711cf87ccbcd601c4d253
Cecilia Pesce <magicalkind@gmail.com>
2023-09-13 22:14:52 -0300

    first commit

---
+++ CupCakes/CupCakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakesApp.swift
+//  CupCakes
+//
+//  Created by Cecilia Andrea Pesce on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CupCakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
