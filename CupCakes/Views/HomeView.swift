a4864178ad679f4c7f30e9b576f8192715d4ff0e
Cecilia Pesce <magicalkind@gmail.com>
2023-09-30 14:22:09 -0300

    fetching cupcakes info from firebase

--- CupCakes/Views/HomeView.swift
+++ CupCakes/Views/HomeView.swift
@@ -8,98 +8,63 @@
 import SwiftUI
 import FirebaseFirestore
 
-let testData = [
-    Cupcake(name: "Banana",
-            price: 10,
-            quantity: 10,
-            image: "banana",
-           description: "Banana diretamente colhida de Joinville"),
-    Cupcake(name: "Limão",
-            price: 10,
-            quantity: 20,
-            image: "lemon",
-           description: "Limão Siciliano"),
-    Cupcake(name: "Chocolate",
-            price: 12,
-            quantity: 5,
-            image: "chocolat",
-            description: "Chocolate Belga")
-]
-
-//Firestore.firestore().collection("cupcakes").addSnapshotListener { (querySnapshot, error) in
-//    guard let documents = querySnapshot?.documents else {
-//        print("No documents")
-//        return
-//    }
-//
-//    documents.map { queryDocumentSnapshot -> Cupcake in
-//        // map document to Book instance here
-//    }
-//}
-    
 struct HomeView: View {
-      var cupcakes = testData
+    @State private var cupcakes = [Cupcake]()
     @State private var cupcakeQuantity = 0
     @State private var cupcakeItem: Cupcake?
     @EnvironmentObject var shoppingCart: ShoppingCart
-
-      // cupcakeQuantity != cupcake.quantity
-      var body: some View {
+    
+    var body: some View {
         NavigationView {
             Form {
                 ForEach(cupcakes) { cupcake in
                     VStack {
                         NavigationLink {
-                            CupcakeDetailsView(cupcakeImage: cupcake.image,
-                                               cupcakeName: cupcake.name,
-                                               description: cupcake.description,
-                                               price: cupcake.price,
-                                               cupcakeQuantity: cupcake.quantity,
-                                               cupcake: cupcakeItem ?? Cupcake(
-                                               name: "Chocolate",
-                                               price: 12,
-                                               quantity: 5,
-                                               image: "chocolat",
-                                               description: "Chocolate Belga"))
+                            CupcakeDetailsView(cupcake: cupcake)
                         } label: {
                             Text(cupcake.name)
                         }
-//                        Stepper("\(cupcake.name)",
-//                                value: $cupcakeQuantity,
-//                                in: 0...cupcake.quantity)
-//                        Text("Total: \(cupcakeQuantity) itens")
                     }
                 }
                 
             }
-          
+            
+        } .onAppear {
+            fetchProducts()
         }
         .navigationBarTitle("Cupcakes")
         .navigationBarItems(trailing:
                                 NavigationLink(destination: ShoppingCartView()) {
             Image(systemName: "cart")
         })
-//          Button {
-//              withAnimation {
-//                  //var action: () -> Void
-//                  //TODO: Chamar tela de carrinho de compras
-//              }
-//          } label: {
-//              VStack {
-//                  Image(systemName: "bag")
-//                      .resizable()
-//                      .scaledToFit()
-//                      .frame(width: 20)
-//                      .padding()
-//                      .background(.black)
-//                      .foregroundColor(.white)
-//                      .fontWeight(.bold)
-//                      .mask(Circle())
-//                  Text("Add to Cart")
-////                      .font(.custom(AppFont.regularFont, size: 10))
-////                      .foregroundColor(RCValues.shared
-////                          .color(forKey: .primary))
-//              }
-//          }
-      }
     }
+    
+    func fetchProducts() {
+        Firestore.firestore().collection("cupcakes").getDocuments() { querySnapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            self.cupcakes = querySnapshot?.documents.compactMap { document -> Cupcake? in
+                let data = document.data()
+                
+                guard let name = data["name"] as? String,
+                      let price = data["price"] as? Int,
+                      let quantity = data["quantity"] as? Int,
+                      let image = data["image"] as? String,
+                      let description = data["description"] as? String
+                else {
+                    return nil
+                }
+                
+                return Cupcake(name: name,
+                               price: price,
+                               quantity: quantity,
+                               image: image,
+                               description: description)
+            } ?? []
+        }
+    }
+}
+
